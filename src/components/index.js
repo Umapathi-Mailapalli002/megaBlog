d9e5cf534ed055a66abe4c943ea74e433a26d11b
Umapathi-Mailapalli002 <ravimailapalli62@gmail.com>
2024-07-10 10:17:12 +0530

    completed

---
+++ src/components/index.js
@@ -0,0 +1,32 @@
+import Header from "./header/Header";
+import Footer from "./footer/Footer";
+import Container from "./container/Container";
+import Logo from "./Logo";
+import LogOutBtn from "./header/LogOutBtn";
+import RTE from "./RTE";
+import Signup from "./Signup"
+import Login from "./Login"
+import PostForm from "./postForm/PostForm"
+import PostCard from "./PostCard"
+import AuthLayout from "./AuthLayout"
+import Button from "./Button"
+import Input from "./Input.jsx"
+import Select from "./Select.jsx"
+
+
+export {
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogOutBtn,
+    RTE,
+    Signup,
+    Login,
+    PostForm,
+    PostCard,
+    AuthLayout,
+    Button,
+    Input,
+    Select
+}
